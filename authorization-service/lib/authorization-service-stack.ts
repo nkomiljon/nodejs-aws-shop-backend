77c1d5cfb0b21ecbd4fe99b4a6eae3c1aedaced9
Komiljon <komiljonnajmitdinov99@gmail.com>
2026-05-30 01:58:47 +0500

    feat: add API Gateway integration and basic authorizer to authorization service

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
 import * as dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,10 +20,39 @@ export class AuthorizationServiceStack extends cdk.Stack {
       },
     });
 
+    const api = new apigateway.RestApi(this, "AuthorizationServiceApi", {
+      restApiName: "Authorization Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: ["GET", "OPTIONS"],
+      },
+    });
+
+    const authorizer = new apigateway.TokenAuthorizer(this, "BasicAuthorizer", {
+      handler: basicAuthorizer,
+      identitySource: apigateway.IdentitySource.header("Authorization"),
+    });
+
+    const authResource = api.root.addResource("auth");
+    authResource.addMethod("GET", new apigateway.MockIntegration({
+      integrationResponses: [{ statusCode: "200" }],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: { "application/json": "{\"statusCode\": 200}" },
+    }), {
+      authorizer,
+      authorizationType: apigateway.AuthorizationType.CUSTOM,
+      methodResponses: [{ statusCode: "200" }],
+    });
+
     new cdk.CfnOutput(this, "BasicAuthorizerArn", {
       value: basicAuthorizer.functionArn,
       description: "Basic Authorizer Lambda ARN",
       exportName: "BasicAuthorizerArn",
+    });
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+      description: "Authorization Service API Gateway URL",
     });
   }
 }
